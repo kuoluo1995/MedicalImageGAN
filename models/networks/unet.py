5f2c0ca99494de84f4064d6b5c37c18afad947e9
kuoluo1995 <kuoluo1995@gmail.com>
2019-08-25 20:24:43 +0800

    临时版本

--- models/networks/unet.py
+++ models/networks/unet.py
@@ -14,32 +14,32 @@ def unet(image, G_channels, out_channels, is_training, reuse=False, name="genera
         e5 = instance_norm(conv2d(leaky_relu(e4), G_channels * 8, name='g_e5_conv'), 'g_bn_e5')
         e6 = instance_norm(conv2d(leaky_relu(e5), G_channels * 8, name='g_e6_conv'), 'g_bn_e6')
         e7 = instance_norm(conv2d(leaky_relu(e6), G_channels * 8, name='g_e7_conv'), 'g_bn_e7')
-        e8 = instance_norm(conv2d(leaky_relu(e7), G_channels * 8, name='g_e8_conv'), 'g_bn_e8')
+        # e8 = instance_norm(conv2d(leaky_relu(e7), G_channels * 8, name='g_e8_conv'), 'g_bn_e8')
 
-        d1 = deconv2d(tf.nn.relu(e8), G_channels * 8, name='g_d1')
+        d1 = deconv2d(tf.nn.relu(e7), G_channels * 8, name='g_d1')
         d1 = tf.nn.dropout(d1, dropout_rate)
-        d1 = tf.concat([instance_norm(d1, 'g_bn_d1'), e7], 3)
+        d1 = tf.concat([instance_norm(d1, 'g_bn_d1'), e6], 3)
 
         d2 = deconv2d(tf.nn.relu(d1), G_channels * 8, name='g_d2')
         d2 = tf.nn.dropout(d2, dropout_rate)
-        d2 = tf.concat([instance_norm(d2, 'g_bn_d2'), e6], 3)
+        d2 = tf.concat([instance_norm(d2, 'g_bn_d2'), e5], 3)
 
         d3 = deconv2d(tf.nn.relu(d2), G_channels * 8, name='g_d3')
         d3 = tf.nn.dropout(d3, dropout_rate)
-        d3 = tf.concat([instance_norm(d3, 'g_bn_d3'), e5], 3)
+        d3 = tf.concat([instance_norm(d3, 'g_bn_d3'), e4], 3)
 
-        d4 = deconv2d(tf.nn.relu(d3), G_channels * 8, name='g_d4')
-        d4 = tf.concat([instance_norm(d4, 'g_bn_d4'), e4], 3)
+        d4 = deconv2d(tf.nn.relu(d3), G_channels * 4, name='g_d4')
+        d4 = tf.concat([instance_norm(d4, 'g_bn_d4'), e3], 3)
 
-        d5 = deconv2d(tf.nn.relu(d4), G_channels * 4, name='g_d5')
-        d5 = tf.concat([instance_norm(d5, 'g_bn_d5'), e3], 3)
+        d5 = deconv2d(tf.nn.relu(d4), G_channels * 2, name='g_d5')
+        d5 = tf.concat([instance_norm(d5, 'g_bn_d5'), e2], 3)
 
-        d6 = deconv2d(tf.nn.relu(d5), G_channels * 2, name='g_d6')
-        d6 = tf.concat([instance_norm(d6, 'g_bn_d6'), e2], 3)
+        d6 = deconv2d(tf.nn.relu(d5), G_channels, name='g_d6')
+        d6 = tf.concat([instance_norm(d6, 'g_bn_d6'), e1], 3)
 
-        d7 = deconv2d(tf.nn.relu(d6), G_channels, name='g_d7')
-        d7 = tf.concat([instance_norm(d7, 'g_bn_d7'), e1], 3)
+        # d7 = deconv2d(tf.nn.relu(d6), G_channels, name='g_d7')
+        # d7 = tf.concat([instance_norm(d7, 'g_bn_d7'), e1], 3)
 
-        d8 = deconv2d(tf.nn.relu(d7), out_channels, name='g_d8')
+        d7 = deconv2d(tf.nn.relu(d6), out_channels, name='g_d7')
 
-        return tf.nn.tanh(d8)
+        return tf.nn.tanh(d7)
