3b28eb5db8c56da36f941a6c8b1009668a720244
kuoluo1995 <kuoluo1995@gmail.com>
2019-09-10 16:18:08 +0800

    fix debug

--- test.py
+++ test.py
@@ -4,18 +4,18 @@ from models import get_model_class_by_name
 from utils.config_utils import get_config
 
 
-def eval(args):
+def _test(args):
     tf.set_random_seed(19)
     tf_config = tf.ConfigProto(allow_soft_placement=True)
     tf_config.gpu_options.allow_growth = True
     with tf.Session(config=tf_config) as sess:
         model_class = get_model_class_by_name(args['model']['name'])
         model = model_class(sess=sess, **args)
-        model.train()
+        model.test()
 
 
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = '1'
     config = get_config('base_pix')
     config['phase'] = 'test'
-    eval(config)
+    _test(config)
