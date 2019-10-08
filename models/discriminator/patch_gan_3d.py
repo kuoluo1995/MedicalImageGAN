c22408d5083df42a14797f5f51b033d9f272ae85
kuoluo1995 <kuoluo1995@gmail.com>
2019-10-08 23:38:48 +0800

    repair 3d unet(patch_gan)

--- models/discriminator/patch_gan_3d.py
+++ models/discriminator/patch_gan_3d.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from models.utils.layers import leaky_relu, instance_norm, conv3d
+from models.utils.layers import leaky_relu, instance_norm3d, conv3d
 
 
 def build_model(image, filter_channels, reuse=False, name='patch_gan', **kwargs):
@@ -7,8 +7,8 @@ def build_model(image, filter_channels, reuse=False, name='patch_gan', **kwargs)
         if reuse:
             tf.get_variable_scope().reuse_variables()
         h0 = leaky_relu(conv3d(image, filter_channels, name='d_h0_cov'))
-        h1 = leaky_relu(instance_norm(conv3d(h0, filter_channels * 2, name='d_h1_conv'), 'd_bn1'))
-        h2 = leaky_relu(instance_norm(conv3d(h1, filter_channels * 4, name='d_h2_conv'), 'd_bn2'))
-        h3 = leaky_relu(instance_norm(conv3d(h2, filter_channels * 8, stride=1, name='d_h3_conv'), 'd_bn3'))
+        h1 = leaky_relu(instance_norm3d(conv3d(h0, filter_channels * 2, name='d_h1_conv'), 'd_bn1'))
+        h2 = leaky_relu(instance_norm3d(conv3d(h1, filter_channels * 4, name='d_h2_conv'), 'd_bn2'))
+        h3 = leaky_relu(instance_norm3d(conv3d(h2, filter_channels * 8, stride=1, name='d_h3_conv'), 'd_bn3'))
         h4 = conv3d(h3, 1, stride=1, name='d_h3_pred')
         return h4
