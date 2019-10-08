7879b6bfbad6fc998279949cb38e80338e3d0224
kuoluo1995 <kuoluo1995@gmail.com>
2019-10-08 23:44:26 +0800

    add summary image

--- models/pix2pix_gan_3d.py
+++ models/pix2pix_gan_3d.py
@@ -53,13 +53,13 @@ class Pix2PixGAN3D(BaseGanModel):
         self.test_metric = self.metrics_fn(self.test_fakeB, self.testB)
 
     def summary(self):
-        # realA_sum = tf.summary.image('{}/{}/{}/AReal'.format(self.dataset_name, self.name, self.tag), self.realA,
-        #                              max_outputs=1)
+        realA_sum = tf.summary.image('{}/{}/{}/AReal'.format(self.dataset_name, self.name, self.tag),
+                                     self.realA[self.image_size[2] // 2], max_outputs=1)
 
         metric_sum = tf.summary.scalar('{}/{}/{}/metricB'.format(self.dataset_name, self.name, self.tag), self.metricB)
         g_loss_A2B_sum = tf.summary.scalar('{}/{}/{}/GLossA2B'.format(self.dataset_name, self.name, self.tag),
                                            self.g_lossA2B)
-        self.g_sum = tf.summary.merge([g_loss_A2B_sum,  metric_sum])
+        self.g_sum = tf.summary.merge([g_loss_A2B_sum, realA_sum, metric_sum])
 
         d_loss_realB_sum = tf.summary.scalar('{}/{}/{}/DLossRealB'.format(self.dataset_name, self.name, self.tag),
                                              self.d_loss_realB)
