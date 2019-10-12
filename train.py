7433dc3bce1e7a91de0385e0417afe66dcb4e62a
kuoluo1995 <kuoluo1995@gmail.com>
2019-10-13 04:13:24 +0800

    repair bug

--- train.py
+++ train.py
@@ -31,6 +31,6 @@ def train(args):
 
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-    config = get_config('base_3d_pix')
+    config = get_config('base_2d_pix')
     config['tag'] = 'basic'
     train(config)
